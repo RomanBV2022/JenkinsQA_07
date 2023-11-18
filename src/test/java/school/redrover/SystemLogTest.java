34ffd3346ca29a35a7f1332e58725e095ad69ae7
PAVEL PASHCHENKOV <142984080+ppashchenkov@users.noreply.github.com>
2023-11-18 12:30:57 +0300

    RF_09.007.02 | (#1793)
    Refactoring SystemLogTest:
    - remove not using method;
    - optimisation testDeleteCustomLogRecorder for quickly execute;

--- src/test/java/school/redrover/SystemLogTest.java
+++ src/test/java/school/redrover/SystemLogTest.java
@@ -2,34 +2,24 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
-
 import java.util.*;
 
 public class SystemLogTest extends BaseTest {
 
     private final static String SYSLOG_NAME = "NewSystemLog";
+
     private void openSyslogPage() {
         JavascriptExecutor js = (JavascriptExecutor) getDriver();
-
         getDriver().findElement(By.xpath("//a[@href='/manage']")).click();
         js.executeScript("arguments[0].scrollIntoView();",
                 getDriver().findElement(By.xpath("//a[@href='log']")));
         getDriver().findElement(By.xpath("//a[@href='log']")).click();
-
-    }
-
-    private boolean isDisplayed(By by) {
-        try {
-            return getDriver().findElement(by).isDisplayed();
-        } catch (NoSuchElementException e) {
-            return false;
-        }
     }
 
     @Test
@@ -54,18 +44,23 @@ public class SystemLogTest extends BaseTest {
 
     @Test(dependsOnMethods ="testCreateCustomLogRecorder")
     public void testDeleteCustomLogRecorder() {
+        List<WebElement> lst = new ArrayList<>();
         openSyslogPage();
 
-        getDriver().findElement(By.xpath("//a[@href='" + SYSLOG_NAME + "/'][1]")).click();
-        getDriver().findElement(By.xpath("//button[@tooltip='More actions']")).click();
-        Actions actions = new Actions(getDriver());
-        actions.pause(400)
-                .moveToElement(getDriver()
-                .findElement(By.xpath("//a[@data-post='true']")))
-                .click()
-                .perform();
-        getWait5().until(ExpectedConditions.alertIsPresent()).accept();
+        do {
+            getDriver().findElement(By.xpath("//a[@href='" + SYSLOG_NAME + "/'][1]")).click();
+            getDriver().findElement(By.xpath("//button[@tooltip='More actions']")).click();
+            Actions actions = new Actions(getDriver());
+            actions.pause(400)
+                    .moveToElement(getDriver()
+                    .findElement(By.xpath("//a[@data-post='true']")))
+                    .click()
+                    .perform();
+            getWait5().until(ExpectedConditions.alertIsPresent()).accept();
 
-        Assert.assertFalse(isDisplayed(By.xpath("//a[@href='" + SYSLOG_NAME + "/'][1]")));
+            lst = getDriver().findElements(By.className("jenkins-table__link"));
+            getDriver().findElement(By.xpath("//*[@id='breadcrumbs']/li[5]/a")).click();
+        } while (lst.size() > 1);
+        Assert.assertEquals(lst.size(),1);
     }
 }
