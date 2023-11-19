de00532b3c0f4fe9af5d917ea1576edd3bdfb199
Olgla <139256463+Olgla@users.noreply.github.com>
2023-11-19 14:35:37 -0500

    Added password generator to SeleniumUtils (#1814)

--- src/test/java/school/redrover/runner/SeleniumUtils.java
+++ src/test/java/school/redrover/runner/SeleniumUtils.java
@@ -3,18 +3,37 @@ package school.redrover.runner;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import school.redrover.runner.BaseTest;
 
+import java.security.SecureRandom;
 import java.util.UUID;
 
 public class SeleniumUtils {
 
     public static String generateRandomName() {
-        String randomName = UUID.randomUUID()
-                .toString()
-                .substring(0, 5);
-        return randomName;
+        return UUID.randomUUID().toString().substring(0, 5);
     }
+
+    public static String generateRandomPassword(int length) {
+        final String charLower = "abcdefghijklmnopqrstuvwxyz";
+        final String charUpper = charLower.toUpperCase();
+        final String number = "0123456789";
+
+        final String randomString = charLower + charUpper + number;
+        SecureRandom random = new SecureRandom();
+
+        if (length <= 0) {
+            throw new IllegalArgumentException("Length must be a positive number");
+        }
+
+        StringBuilder randomPassword = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            int rndCharAt = random.nextInt(randomString.length());
+            char rndChar = randomString.charAt(rndCharAt);
+            randomPassword.append(rndChar);
+        }
+        return randomPassword.toString();
+    }
+
 
     public static void jsClick(WebDriver driver, WebElement element) {
         JavascriptExecutor executor = (JavascriptExecutor) driver;
