02c22c0db379d139126c736c7117c79d2a449945
PAVEL PASHCHENKOV <142984080+ppashchenkov@users.noreply.github.com>
2023-11-20 12:57:27 +0300

    Pp/tc 09.007.02 (#1835)
    * add confIXBT test
    
    * TC_09.007.02 | Manage Jenkins > System log
    Delete All custom log recorders
    
    add testDeleteAllCustomLogRecorders()

--- src/test/java/school/redrover/SystemLogTest.java
+++ src/test/java/school/redrover/SystemLogTest.java
@@ -42,7 +42,7 @@ public class SystemLogTest extends BaseTest {
                 .getText(), SYSLOG_NAME);
     }
 
-    @Test(dependsOnMethods ="testCreateCustomLogRecorder")
+    @Test(dependsOnMethods = "testCreateCustomLogRecorder")
     public void testDeleteCustomLogRecorder() {
         List<WebElement> lst = new ArrayList<>();
         openSyslogPage();
@@ -61,6 +61,30 @@ public class SystemLogTest extends BaseTest {
             lst = getDriver().findElements(By.className("jenkins-table__link"));
             getDriver().findElement(By.xpath("//*[@id='breadcrumbs']/li[5]/a")).click();
         } while (lst.size() > 1);
+        Assert.assertEquals(lst.size(),1);
+    }
+
+    @Test
+    public void testDeleteAllCustomLogRecorders() {
+        List<WebElement> lst = new ArrayList<>();
+        openSyslogPage();
+
+        lst = getDriver().findElements(By.className("jenkins-table__link"));
+        if(lst.size() > 1) {
+            do {
+                lst.get(1).click();
+                getDriver().findElement(By.xpath("//button[@tooltip='More actions']")).click();
+                Actions actions = new Actions(getDriver());
+                actions.pause(400)
+                        .moveToElement(getDriver()
+                        .findElement(By.xpath("//a[@data-post='true']")))
+                        .click()
+                        .perform();
+                getWait5().until(ExpectedConditions.alertIsPresent()).accept();
+                getDriver().findElement(By.xpath("//*[@id='breadcrumbs']/li[5]/a")).click();
+                lst = getDriver().findElements(By.className("jenkins-table__link"));
+            } while (lst.size() > 1);
+        }
         Assert.assertEquals(lst.size(),1);
     }
 }
