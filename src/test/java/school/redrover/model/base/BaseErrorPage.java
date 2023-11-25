47e5f32a16abb41a9823ad2bad30a4e83d9e1c31
AlekseiChapaev <93160447+AlekseiChapaev@users.noreply.github.com>
2023-11-25 21:25:31 +0100

    Changed heritage BaseErrorPage from BasePage (#1933)

--- src/test/java/school/redrover/model/base/BaseErrorPage.java
+++ src/test/java/school/redrover/model/base/BaseErrorPage.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public abstract class BaseErrorPage extends BaseModel {
+public abstract class BaseErrorPage extends BasePage {
 
     @FindBy(xpath = "//h1")
     private WebElement errorNotification;
